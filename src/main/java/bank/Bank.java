ca173031ae9ff0fb7d04f845d7703a5a06f76f20
Art Weinstein <artur.weinstein@gmail.com>
2022-01-10 09:04:25 -0800

    add functionality to create and authenticate an account

--- src/main/java/bank/Bank.java
+++ src/main/java/bank/Bank.java
@@ -1,4 +1,33 @@
 package main.java.bank;
 
+import java.util.Scanner;
+
 public class Bank {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the bank! Please register an account.");
+
+
+        System.out.println("Enter a name");
+        String name = scan.nextLine();
+        System.out.println(name);
+
+        System.out.println("Enter a password");
+        String password = scan.nextLine();
+        System.out.println(password);
+
+
+        System.out.println("Please log in using your credentials.");
+        System.out.println("Username");
+        String nameInput = scan.nextLine();
+        System.out.println("Password");
+        String passwordInput = scan.nextLine();
+        if(name.equals(nameInput) && password.equals(passwordInput)) {
+            System.out.println("Welcome " + name);
+        } else {
+            System.out.println("Please enter a valid username or password");
+        }
+    }
 }
